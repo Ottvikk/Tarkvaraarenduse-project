47a7f9b38a233b0ec5f590affa2c95edf80c8d75
Ottvikk <ott-saamuel.oja@vikk.ee>
2023-01-20 13:21:46 +0200

    Tööd

--- Tekstide ja piltide kasutamine.py
+++ Tekstide ja piltide kasutamine.py
@@ -23,10 +23,25 @@ pygame.init()
 #ekraani seaded
 screen=pygame.display.set_mode([640,480])
 pygame.display.set_caption("Harjutamine")
-screen.fill([204, 255, 204])
+
 
 #Lisame pildid
-bg = pygame.image.load("img/bg_shop.jpg")
-screen.blit(bg,[0,0])
-
+bg_shop = pygame.image.load("img/bg_shop.jpg")
+screen.blit(bg_shop,[0,0])
+bg_shop = pygame.transform.scale(bg_shop, [300, 120])
+seller = pygame.image.load("img/seller.jpg")
+seller = pygame.transform.scale(seller, [200, 270])
+screen.blit(seller,[200,150])
+chat = pygame.image.load("img/chat.jpg")
+chat = pygame.transform.scale(chat, [250, 170])
+screen.blit(chat,[350,30])
+font = pygame.font.Font(None, 25)
+text = font.render("Tere, olen Ott-Saamuel Oja", True, [255,255,255])
+screen.blit(text, [360,100])
 pygame.display.flip()
+
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
