56bbed876648260ad504f0045d1e8258480612c2
Ottvikk <ott-saamuel.oja@vikk.ee>
2023-02-06 11:33:32 +0200

    Tööd

--- For-tsükkel.py
+++ For-tsükkel.py
@@ -15,11 +15,25 @@ for i in range(5):
 """"""
 for i in range (1,5, 1):
     print(i)
-"""
+""""""
 arvud = [2,4,6,7,2]
 for i in range(0,5,1):
     print(arvud[i])
-"""
+""""""
+arvud = [2,4,6,7,2,99]
+for i in range(0,6,5):
+    print(arvud[i])
+""""""
+arvud = [2,4,6,7,2]
+arvud.append(5)
+arvud[5] = 99
+arvud.remove(2)
+arvud.append(5)
+arvud[5] = 1
+arvud.sort()
+for i in range(0,6,1):
+    print(arvud[i])
+""""""
 a = []
 a.append(2)
 """"""
