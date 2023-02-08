cf36147a30161d4b6ee86775445c5e1a98e82a10
Ottvikk <ott-saamuel.oja@vikk.ee>
2023-02-08 10:37:03 +0200

    Tööd

---
+++ Ül 1-9 _Ott-Saamuel Oja.py
@@ -0,0 +1,41 @@
+"""
+#Ül 1
+a = 1
+b = "a/n"
+while a < 3:
+    print(b)
+    a +=1
+"""
+"""
+#Ül2
+b = "'Tere!'"
+c = '"Head aega!"'
+print(b)
+print("/n")
+print(c)
+"""
+"""
+#Ül 3
+a = "Tere"
+b = "Tore"
+lause = a * 3 + "//" + b
+print(lause)
+"""
+"""
+#Ül 4
+nimi = "loomariik"
+print(nimi[0] + nimi[-1])
+"""
+"""
+#Ül 5
+nimi1 = "Paide"
+nimi2 = "Türi"
+print("Tere,"[0],nimi1[-5],nimi2,"!")
+"""
+
+#Ül 6
+a = "A"
+b = 1
+while b < 3:
+    print(a,end=" ")
+    b += 1
