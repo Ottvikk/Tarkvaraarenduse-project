d15ccea02855b712081fadac3f073fb930eaba8e
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-02-14 15:42:45 +0200

    kodu

---
+++ sammud.py
@@ -0,0 +1,13 @@
+f = open('sammud.txt')
+
+kogus = []
+while True:
+    nimi = f.readline()
+    print(nimi)
+    if nimi == "":
+        break
+kogus = kogus.append(7) = nimi
+print(len(nimi))
+print(kogus)
+
+f.close()
