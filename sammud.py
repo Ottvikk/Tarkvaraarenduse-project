6c6db0a57a8ebfb4195b58f3718ae4a517a4d531
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-02-16 16:02:12 +0200

    kodu

--- sammud.py
+++ sammud.py
@@ -1,23 +1,8 @@
-"""
-f = open('sammud.txt')
 
-kogus = 0
-while True:
-    nimi = f.readline()
-    print(nimi)
-    if nimi == "":
-        break
-
-print(len(nimi))
-print(kogus)
-
-f.close()
-"""
-"""
-myfile = 'sammud.txt'
+sammud = 'sammud.txt'
 sum = 0
 a = 0
-with open(myfile) as fh:
+with open(sammud) as fh:
     for n in fh:
         n = n.strip()
         sum = sum + int(n)
@@ -26,21 +11,46 @@ with open(myfile) as fh:
 print('Päevade peale oli samme kokku ' + str(sum))
 print('Kokku liikusid ' + str(a) + ' Päeva')
 print('Päeva keskmine sammude arv on ' + str(sum1))
-"""
-b = open('sammud.txt', 'r')
-
-c = b.readlines()
-regels = len(c)
-
-print(regels)
-
-max = 0
-for line in b.readlines():
-  num = int(line.split(",")[0])
-  if (max < num):
-    max = num
 
 
-print(max)
-# Close file
-b.close()
+ln = 0
+maxln = 0
+maxn = 0
+with open('sammud.txt', 'r') as f:
+    line = f.__next__()
+    if line:
+       ln = 1
+       maxln = 1
+       maxn = int(line.split(",")[0].strip())
+    else:
+       raise Exception('Empty content')
+    for line in f:
+        ln += 1
+        cur = int(line.split(",")[0].strip())
+        if cur > maxn:
+            maxn = cur
+            maxln = ln
+print (str(maxln) + ' päeval oli sammude arv suurim')
+print ('Suurim sammude arv oli ' + str(maxn)) #result
+
+
+ln = 0
+lowln = 0
+lown = 0
+with open('sammud.txt', 'r') as f:
+    line = f.__next__()
+    if line:
+       ln = 1
+       lowln = 1
+       lown = int(line.split(",")[0].strip())
+    else:
+       raise Exception('Empty content')
+    for line in f:
+        ln += 1
+        cur = int(line.split(",")[0].strip())
+        if cur < lown:
+            lown = cur
+            lowln = ln
+print (str(lowln) + ' päeval oli sammude arv väikseim')
+print ('Väikseim sammude arv oli ' + str(lown)) #result
+
