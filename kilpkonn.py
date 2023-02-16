64bf0a0f64e34a21e6d6b473fa37d92a557237f8
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-02-16 16:28:17 +0200

    kodu

--- kilpkonn.py
+++ kilpkonn.py
@@ -1,11 +1,13 @@
 f = open('kilpkonn.txt')
-import turtle
+from turtle import *
 korrad = input("Sisesta mitu korda tegema peab: ")
 while True:
+    for i in range(int(korrad)):
+       tutle = t.square(100)
     nimi = f.readline()
-    print(nimi)
+    print('turtle.' + nimi)
+
     if nimi == "":
         break
-
 
 f.close()
