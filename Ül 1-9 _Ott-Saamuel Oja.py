1e248d9b5ededb23eef304b4b3304ffa7bcb2b6a
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-02-27 11:10:34 +0200

    kodu

--- Ül 1-9 _Ott-Saamuel Oja.py
+++ Ül 1-9 _Ott-Saamuel Oja.py
@@ -32,10 +32,38 @@ nimi1 = "Paide"
 nimi2 = "Türi"
 print("Tere,"[0],nimi1[-5],nimi2,"!")
 """
-
+"""
 #Ül 6
 a = "A"
 b = 1
 while b < 3:
     print(a,end=" ")
     b += 1
+"""
+"""
+#Ül 7
+a = 10
+b = 2
+print(a > b)
+"""
+"""
+#Ül 8
+a = 10
+b = 2
+print(a != b and b > 10)
+print(b == a or a >= 0)
+"""
+"""
+#Ül 9
+from tkinter import *
+raam = Tk()
+raam.title("Tühi tahvel")
+tahvel = Canvas(raam, width=600)
+
+tahvel.create_rectangle(50,70,100,100, width=2, outline="blue")
+tahvel.create_text(50,50, text="Tere!")
+
+tahvel.create_polygon(100,100,150,150,200,100, fill="red",outline="black")
+tahvel.pack()
+raam.mainloop()
+"""
