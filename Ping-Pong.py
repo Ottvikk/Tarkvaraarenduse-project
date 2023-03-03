bc7c3a3537bf0c6cd516ccc33b93ac7c5e60f43f
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-03-03 14:02:59 +0200

    kodu

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,28 +1,166 @@
-import pygame
+
+import pygame, sys
+
+pygame.init()
+
+# värvid
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+pink = [255, 153, 255]
+lGreen = [153, 255, 153]
+lBlue = [153, 204, 255]
+
+# ekraani seaded
+screenX = 640
+screenY = 480
+screen = pygame.display.set_mode([screenX, screenY])
+pygame.display.set_caption("Animeerimine")
+screen.fill(lBlue)
+clock = pygame.time.Clock()
+
+balls = []
+score = 0
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, red)
+    dis.blit(value, [0, 0])
 
 
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-light_blue = (138, 235, 255)
 
-y = 480
-x = 640
-screen = pygame.display.set_mode((x , y))
-screen.fill(light_blue)
-pygame.display.set_caption("Ping-Pong, Ott-Saamuel Oja")
+# graafika laadimine
+ball = pygame.image.load("img/ball.png")
+pad = pygame.image.load("img/pad.png")
+
+# kiirus ja asukoht
+posX, posY = 0, 0
+speedX, speedY = 7, 4
+posA, posB = 320, 0
+speedA, speedB = 0, 3.7
+gameover = False
+while not gameover:
+    # fps
+    clock.tick(60)
+    # mängu sulgemine ristist
+    events = pygame.event.get()
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+
+    # pildi lisamine ekraanile
+    screen.blit(ball, (posX, posY))
+    screen.blit(pad, (posB, posA))
+    posX += speedX
+    posY += speedY
+    posB += speedB
+    posA += speedA
+    # kui puudub ääri, siis muudab suunda
+    if posX > screenX - ball.get_rect().width or posX < 0:
+        speedX = -speedX
+
+    if posY > screenY - ball.get_rect().height or posY < 0:
+        speedY = -speedY
+
+    if posX > screenX - pad.get_rect().width or posX < 10:
+        speedA = -speedA
+
+    if posY > screenY - pad.get_rect().height or posY < 10:
+        speedB = -speedB
+
+    for ball in balls[:]:
+        if pad.colliderect(ball):
+            balls.remove(pad    )
+            score += 1
 
 
-
-pygame.display.update()
-running=True
-# Hakkab tsükkel. Selle abil saab panna X ekraani kinni.
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # graafika kuvamine ekraanil
+    pygame.display.update()
+    pygame.display.flip()
+    screen.fill(lBlue)
 
 pygame.quit()
+"""
+import pygame, random
+pygame.init()
+
+#värvid
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
+pink = [255, 153, 255]
+lGreen = [153, 255, 153]
+lBlue = [153, 204, 255]
+
+#ekraani seaded
+screenX = 640
+screenY = 480
+screen=pygame.display.set_mode([screenX,screenY])
+pygame.display.set_caption("Surface")
+screen.fill(lBlue)
+clock = pygame.time.Clock()
+posX, posY = 0, 0
+speedX, speedY = 3, 4
+
+#player
+player = pygame.Rect(posX, posY, 120, 140)
+playerImage = pygame.image.load("img/ball.png")
+playerImage = pygame.transform.scale(playerImage, [player.width, player.height])
+
+#enemy - tekitame 5 suvalist vaenlast
+enemies = []
+for i in range(5):
+    enemies.append(pygame.Rect(random.randint(0, screenX - 100), random.randint(0, screenY - 100), 60, 73))
+enemyImage = pygame.image.load('img/pad.png')
+enemyImage = pygame.transform.scale(enemyImage, [enemies[0].width, enemies[0].height])
+
+
+enemyCounter = 0
+totalEnemies = 20
+score = 0
+
+gameover = False
+while not gameover:
+    clock.tick(60)
+    #mängu sulgemine ristist
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        break
+
+    #player liikumine
+    player = pygame.Rect(posX, posY, 120, 140)
+    screen.blit(playerImage,player)
+
+    posX += speedX
+    posY += speedY
+
+    if posX > screenX-playerImage.get_rect().width or posX < 0:
+        speedX = -speedX
+
+    if posY > screenY-playerImage.get_rect().height or posY < 0:
+        speedY = -speedY
+
+
+    #enemy loomine
+    enemyCounter += 1
+    if enemyCounter >= totalEnemies:
+        enemyCounter = 0
+        enemies.append(pygame.Rect(random.randint(0, screenX - 100), random.randint(0, screenY - 100), 60, 73))
+
+    for enemy in enemies[:]:
+        if player.colliderect(enemy):
+            enemies.remove(enemy)
+            score += 1
+
+    for enemy in enemies:
+        screen.blit(enemyImage, enemy)
+
+    pygame.display.flip()
+    screen.fill(lBlue)
+
+    print(score)
+    if score == 20:
+        gameover = True
+pygame.quit()
+"""
