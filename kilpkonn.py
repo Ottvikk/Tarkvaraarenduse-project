d0ba49a239b865a9d595b05f4684c9ccc595da47
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-03-06 09:27:16 +0200

    kodu

--- kilpkonn.py
+++ kilpkonn.py
@@ -3,7 +3,7 @@ from turtle import *
 korrad = input("Sisesta mitu korda tegema peab: ")
 while True:
     for i in range(int(korrad)):
-       tutle = t.square(100)
+       turtle = turtle.square(100)
     nimi = f.readline()
     print('turtle.' + nimi)
 
