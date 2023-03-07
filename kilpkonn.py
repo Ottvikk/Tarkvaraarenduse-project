36824228f35c85c437b4d02968aa911c39f4f891
Ottvikk <ott-saamuel.oja@vikk.ee>
2023-03-07 11:34:44 +0200

    Tööd

--- kilpkonn.py
+++ kilpkonn.py
@@ -1,12 +1,26 @@
-f = open('kilpkonn.txt')
-from turtle import *
-korrad = input("Sisesta mitu korda tegema peab: ")
-while True:
-    for i in range(int(korrad)):
-        nimi = f.readline()
-        print('turtle.' + nimi)
+import turtle #impordib turtle
 
-    if nimi == "":
-        break
+pcolor = ("black")
+kilpkonna_juhised = [] #loob järjendi
 
-f.close()
+f = open('kilpkonn.txt','r') #avab tekstidokumendi juhistega
+print(f.read()) #loeb neid juhiseid
+for rida in f:
+    kilpkonna_juhised.append(rida.strip)
+
+# tõlgib eestikeelsed juhendid turtleile arusaadavaks
+def joonista_kujund(kujund, kilpkonn):
+    for käsk in kujund:
+        if käsk[0] == "edasi":
+            kilpkonn.forward(int(käsk[1]))
+        elif käsk[0] == "tagasi":
+            kilpkonn.backward(int(käsk[1]))
+        elif käsk[0] == "paremale":
+            kilpkonn.right(int(käsk[1]))
+        elif käsk[0] == "vasakule":
+            kilpkonn.left(int(käsk[1]))
+
+
+print(kilpkonna_juhised) #väljastab konsooli juhised
+
+turtle.exitonclick() #sulgeb mängu ekraanile vajutades
