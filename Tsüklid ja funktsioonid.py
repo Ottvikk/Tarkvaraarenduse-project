087fd06895c6a6c820fca6678e4da94568120674
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-03-09 22:34:15 +0200

    kodu

--- Tsüklid ja funktsioonid.py
+++ Tsüklid ja funktsioonid.py
@@ -1,3 +1,4 @@
+"""
 import pygame
 pygame.init()
 
@@ -28,3 +29,45 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+"""
+import pygame #Impordib pygame'i
+
+punane = [255, 0, 0] #Värvid
+kollane = [255, 255, 51]
+sinine = [0, 0, 255]
+valge = [0, 0, 0]
+must = [255, 255, 255]
+
+pygame.display.set_caption("Ruudud")
+screen = pygame.display.set_mode([640, 480]) #Ekraani seaded
+
+screen.fill(punane) #Ekraani värvi muutmine
+
+class ruut: #Teeme klassi nimega ruut sest selle abil me teeme ruudud
+    def __init__(self, color, esimene, teine): #Defineerime self'i, color'i, esimese ja teise
+        self.color = color
+        self.esimene = esimene
+        self.teine = teine
+
+    def make_square(self): #Defineerime sõna make_square
+        y = 1
+        for i in range(35):
+            x = 1
+            for j in range(38):
+                pygame.draw.rect(screen, self.color, [x, y, self.esimene, self.teine])
+                x += 18
+            y += 18
+
+ruut.make_square(ruut(sinine, 16, 16)) #Joonistab ruudud
+
+pygame.display.update() #Uuendab ekraani
+
+running = True #Tsükkel mis hoiab kasutajal pilti ees
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update() #Uuendab ekraani
+
+pygame.quit() #Kinni panemiseks
