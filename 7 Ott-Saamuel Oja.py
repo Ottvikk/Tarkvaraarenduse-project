1168c51ef41025fe329b0ce9e1ff6fcb127bd2de
OttVikk <Ott-saamuel.oja@vikk.ee>
2023-03-20 18:06:21 +0200

    kodu

---
+++ 7 Ott-Saamuel Oja.py
@@ -0,0 +1,107 @@
+"""
+tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
+print(tunniplaan)
+"""
+"""
+tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
+print(tunniplaan[0])
+"""
+"""
+tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
+print(len(tunniplaan))
+"""
+"""
+tunniplaan = ["mate", "inka", "vene", "kunst", "arvuti"]
+print("kunst"in tunniplaan)
+"""
+"""
+a = [1, 12, -3, 4, 0]
+for i in range(1,5,1): 
+    print(a[i])
+"""
+"""
+a = [1, 12, -3, 4, 0]
+for i in range(1,4,1): 
+    print(a[i])
+"""
+"""
+a = [1, 12, -3, 4, 0]
+print(min(a))
+"""
+"""
+a = [1, 12, -3, 4, 0]
+a.sort()
+print(a)
+"""
+"""
+a = [1, 12, -3, 4, 0]
+a.append(5)
+a[5]=9
+print(a)
+"""
+"""
+a = [1, 12, -3, 4, 0]
+a[0]=5
+print(a)
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+print(a+b)
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+c = (a+b)
+c = b.extend([4])
+print(c)
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+print(sum(b))
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+o=0
+while o<3:
+       print(a[o])
+       o +=1
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+"""
+"""
+a = ['A', 'B', 'C']
+b = [1, 2, 3]
+h=0
+while h<3:
+    print(b[h])
+    h +=1
+"""
+"""
+a=[]
+for b in range(3):
+    a.append(b)
+print(a)
+"""
+"""
+for i in range(2,5,1):
+    print(i)
+"""
+"""
+for i in range(2,7,2):
+    print(i)
+"""
+"""
+for i in range(-1,-4,-2):
+    print(i)
+"""
+"""
+korrutis = 15
+for i in range(1,6,2):
+    korrutis =3*i
+    print(korrutis)
+"""
